01d4624dd6ec7f6e6a8b557590f7ba9ad283fbf6
TienxDun <tomboytmn@gmail.com>
2026-04-07 21:30:21 +0700

    refactor: remove XML comments from IPaymentService interface

--- Interfaces/Payment/IPaymentService.cs
+++ Interfaces/Payment/IPaymentService.cs
@@ -4,24 +4,16 @@ namespace API_DigiBook.Interfaces.Payment
 {
     public interface IPaymentService
     {
-        /// <summary>
         /// Tạo link thanh toán
-        /// </summary>
         Task<PaymentResponse> CreatePaymentLinkAsync(PaymentRequest request);
 
-        /// <summary>
         /// Xác thực thanh toán
-        /// </summary>
         Task<PaymentVerification> VerifyPaymentAsync(string orderId);
 
-        /// <summary>
         /// Xử lý callback từ payment gateway
-        /// </summary>
         Task<bool> HandleCallbackAsync(Dictionary<string, string> callbackData);
 
-        /// <summary>
         /// Lấy tên provider
-        /// </summary>
         string GetProviderName();
     }
 }
